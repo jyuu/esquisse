61377012f6d801e7239ca399e46f645389a08393
jyuu <jyu21@ncsu.edu>
2019-08-21 19:53:03 -0400

    Module for extracting code

--- R/module-pivot-code.R
+++ R/module-pivot-code.R
@@ -1,15 +1,15 @@
+
 pivotCodeUI <- function(id, insert_code = TRUE) {
   ns <- NS(id)
-  
   tagList(
     tags$button(
       class = "btn btn-default btn-xs pull-right btn-copy-code",
       "Copy to clipboard", `data-clipboard-target` = paste0("#", ns("codetidyr"))
-    ), tags$script("new Clipboard('.btn-copy-code');"),
+    ), 
+    tags$script("new Clipboard('.btn-copy-code');"),
     tags$br(),
     tags$b("Code:"),
     uiOutput(outputId = ns("code")),
-    tags$textarea(id = ns("holderCode"), style = "display: none;"),
     if (insert_code) {
       actionLink(
         inputId = ns("insert_code"),
@@ -22,10 +22,20 @@ pivotCodeUI <- function(id, insert_code = TRUE) {
 }
 
 pivotCodeServer <- function(input, output, session,
-                            tidyr_code) {
+                            code_expr = NULL) {
+  ns <- session$ns
+  
+  observeEvent(input$insert_code, {
+    context <- rstudioapi::getSourceEditorContext()
+    code <- code_expr
+    if (input$insert_code == 1) {
+      code <- paste("library(tidyr)", code, sep = "\n\n")
+    }
+    rstudioapi::insertText(text = paste0("\n", code), id = context$id)
+  })
+  
   output$code <- renderUI({
-    code <- tidyr_code$code
-    
+    code <- code_expr
     htmltools::tagList(
       rCodeContainer(id = ns("codetidyr"), code)
     )
