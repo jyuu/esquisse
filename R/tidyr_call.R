403e9e854f42d9e8195ff74180f6e73b2307d396
jyuu <jyu21@ncsu.edu>
2019-08-21 19:53:52 -0400

    Working tidyr call

--- R/tidyr_call.R
+++ R/tidyr_call.R
@@ -3,52 +3,71 @@
 #' @param data A \code{reactiveValues} with at least a slot \code{data} 
 #'   containing a \code{data.frame} to use in the module. And a slot 
 #'   \code{name} corresponding to the name of the \code{data.frame}.
-#' @param targets_fix Desired variables to fix in the \code{data.frame}. 
-#' @param targets_pivot Desired variables to pivot on in the \code{data.frame}. 
-#'   Note, if \code{targets_fix} is provided, the \code{target_pivots} 
-#'   need not be. 
+#' @param targets Desired variables to pivot on in the \code{data.frame}. 
 #' @param settings Desired pivot settings as selected by the user. 
 #' 
 #' @return An expression that can be parsed.
 #' 
 #' @export
 #' 
-#' @importFrom rlang syms sym is_string expr
+#' @importFrom rlang expr
+#' @importFrom tidyr pivot_longer pivot_wider
 tidyr_call <- function(data = NULL,
-                       targets_fix = NULL,
-                       targets_pivot = NULL,
+                       targets= NULL,
                        settings = NULL) {
   
   if (is.null(data)) {
     return("")
   }
   
-  data <- sym(data)
+  data <- rlang::sym(data)
   
-  targets_fix <- dropNulls(targets_fix)
-  targets_pivot <- dropNulls(targets_pivot)
-  
-  cols <- expr(-c(!!!syms(targets_fix)))
-  
-  if (settings$pivot_type == "longer") {
-    settings <- syms(settings)
-    call <- expr(
-      pivot_longer(
-        data = !!data,
-        cols = !!cols,
-        names_to = paste0(expr(!!settings$names_column)),
-        values_to = paste0(expr(!!settings$values_column))
-      )
-    )
-  } else {
-    settings <- syms(settings)
-    call <- expr(
-      pivot_wider(
-        data = !!data,
-        names_from = paste0(expr(!!settings$names_column)),
-        values_from = paste0(expr(!!settings$values_column))
-      )
-    )
+  syms2 <- function(x) {
+    if (rlang::is_string(x)) {
+      return(rlang::sym(x))
+    } else {
+      return(x)
+    }
   }
-  call
+  
+  targets <- dropNulls(targets)
+  
+  cols <- expr(c(!!!rlang::syms(targets)))
+  
+  if (rlang::is_null(settings)) {
+    call <- "No call can be produced yet."
+  } else {
+    if (settings$pivot_type == "longer") {
+      settings <- lapply(settings, syms2)
+      call <- expr(
+        pivot_longer(
+          data = !!data,
+          cols = !!cols,
+          names_to = rlang::as_string(expr(!!settings$names_column)),
+          values_to = rlang::as_string(expr(!!settings$values_column)),
+          values_drop_na = !!settings$drop_na 
+        )
+      )
+    } else if (settings$pivot_type == "wider") {
+      settings <- lapply(settings, syms2)
+      call <- expr(
+        pivot_wider(
+          data = !!data,
+          names_from = rlang::as_string(expr(!!settings$names_column)),
+          values_from = rlang::as_string(expr(!!settings$values_column))
+        )
+      )
+    } else {
+      call <- "No call can be produced yet."
+    }
+  }
+  
+  # Evaluate call 
+  call_result <- try(eval(call), silent = TRUE)
+  
+  if (inherits(call_result, "try-error")) {
+    call_result <- "Output transformation is not possible."
+  }
+  
+  return(list(call = call, call_result = call_result))
 }
