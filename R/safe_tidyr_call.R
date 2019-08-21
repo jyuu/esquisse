629335fac863cc0cca6f45c5ce16609839b5b9b0
jyuu <jyu21@ncsu.edu>
2019-08-21 19:53:45 -0400

    Deleted safe tidyr call

--- R/safe_tidyr_call.R
+++
@@ -1,9 +0,0 @@
-
-safe_tidyr_call <- function(expr, data = NULL) {
-  if(!is.null(data)) {
-    output <- rlang::eval_tidy(expr = expr, data = data)
-  } else {
-    output <- mtcars
-  }
-  return(output)
-}
